226482dac56d23e0aaedb3c035323235ec73b070
shantanu-nikumbh <shantanupnikumbh17@gmail.com>
2026-01-28 19:51:56 -0800

    Added react app

---
+++ react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Greet from './components/Greet'
+
+function App() {
+
+
+  return (
+    <>
+     <Greet></Greet>
+    </>
+  )
+}
+
+export default App
---
+++ react-app/src/components/Greet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greet() {
+  return (
+    <>
+    <h1>Welcome to React!!</h1>
+    </>
+  )
+}
+
+export default Greet
