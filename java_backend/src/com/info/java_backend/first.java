1a4dd9d2abfd48b5f55c6c521835cf9d7016ef96
shantanu-nikumbh <shantanupnikumbh17@gmail.com>
2026-01-29 20:49:38 -0800

    add java folder

---
+++ java_backend/src/com/info/java_backend/first.java
@@ -0,0 +1,10 @@
+package com.info.java_backend;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+	}
+
+}
